adab864e45ca58b10998eb63864d6a923bc2ceb3
josh-parris <github.josh_parris@spamgourmet.com>
2015-08-29 22:25:37 +1000

    many bugfixes
    Usage output narrowed to fit in turtle's window; misnamed function fixed; misparsed params fixed

--- examples/tube.lua
+++ examples/tube.lua
@@ -45,7 +45,7 @@ function MineHorizontalTunnelTwoSteps()
   LineTunnelDown()
   TurnAnti()
   LineTunnelForward(TurnAround)
-  LineTunnelForward(Noop)
+  LineTunnelForward(NoOp)
   -- First Step, top block
   MineUp()
   LineTunnelForward(TurnAnti)
@@ -59,7 +59,7 @@ function MineHorizontalTunnelTwoSteps()
   LineTunnelUp()
   TurnAnti()
   LineTunnelForward(TurnAround)
-  LineTunnelForward(Noop)
+  LineTunnelForward(NoOp)
   -- Second Step, bottom block
   MineDown()
   LineTunnelForward(TurnAnti)
@@ -170,7 +170,7 @@ end
 
 function MakeTubeBy(tubeFn, times)
   for n=1,times do
-    if haltFn then
+    if haltFn() then
       print("Storage full. Halting after " .. n .. " steps")
       break
     end
@@ -178,31 +178,35 @@ function MakeTubeBy(tubeFn, times)
   end
 end
 
+function usage()
+  print( "Usage: tube <up|dn|fwd|fast|stairdn|stairup>" )
+  print( "            <length> [dump|noore]" )
+  print( "Makes a sealed passageway, for travelling" )
+  print( "through lava, gases and hostile environments" )
+  print( "  fwd makes a two high, 1 wide sealed tunnel" )
+  print( "  fast does the same but unsealed" )
+  print( "  up makes minimum height stairs" )
+  print( "  stairup makes 5-high staircase for steps" )
+  print( "  unless "dump", will stop when full" )
+  print( "  noore causes orebodies to be not followed" )
+end
+
 -- Main routine
 local tArgs = { ... }
-if #tArgs ~= 2 then
-	print( "Usage: tube <up|dn|fwd|fast|stairdn|stairup> <length>" )
-	print( "            [dump|noore]" )
-  print( "  Makes a sealed passageway, useful for travelling" )
-  print( "  through lava, gases and other hostile environments -" )
-  print( "  fwd makes a two high, 1 wide sealed tunnel" )
-  print( "  fast does the same but doesn't bother sealing" )
-  print( "  up makes minimum height stairs which bump your head" )
-  print( "  stairup makes 5-high staircase with space for steps" )
-  print( "  unless dump is specified, will stop when full" )
-  print( "  noore causes orebodies to be ignored rather than mined" )
-	return
+if #tArgs == 1 or #tArgs > 3 then
+  usage()
+  return
 end
 
 local length = tonumber( tArgs[2] )
 if length < 1 then
-	print( "Tunnel length must be positive" )
-	return
+  print( "Tunnel length must be positive" )
+  return
 end
 if length > 64 then
   -- because, chunk unloading and stuff
-	print( "Tunnel can't be more than 64 blocks long" )
-	return
+  print( "Tunnel can't be more than 64 blocks long" )
+  return
 end
 
 if tArgs[3] == 'noore' then
@@ -211,6 +215,8 @@ elseif tArgs[3] == 'dump' then
   haltFn = NoOp
 elseif tArgs[3] ~= nil then
   print("I don't know " .. tArgs[3])
+  usage()
+  return
 end
 
 if tArgs[1] =='up' then
@@ -229,4 +235,5 @@ elseif tArgs[1] == 'fast' or tArgs[1] == 'ff' then
   MakeTubeBy(MineHorizontalTunnelFast, length)
 else
   print("I don't know " .. tArgs[1])
+  usage()
 end
